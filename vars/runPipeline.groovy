7765bc4b324db96ba412bee74654135bd043ff45
Vikas Pogu <vikaspoguadf@gmail.com>
2020-07-25 12:07:42 -0500

    dont fail on anchore

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -42,7 +42,7 @@ def call(body) {
       stage('analyze') {
         steps {
             sh "echo 'docker.io/vikaspogu/${imageName} Dockerfile' > anchore_images"
-            anchore name: 'anchore_images'
+            anchore name: 'anchore_images', bailOnFail: false
         }
       }
       stage('teardown') {
