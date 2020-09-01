a51734568c00bbc430fb3d6bc1ab43b26317d288
vikaspogu <vikaspoguadf@gmail.com>
2020-09-01 14:48:26 -0500

    try buildx

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -28,7 +28,9 @@ def call(body) {
       stage("build") {
         steps{
           container("docker") {
-              sh "docker run --rm --privileged multiarch/qemu-user-static:register --reset -p yes"
+              sh "docker run --rm --privileged multiarch/qemu-user-static --reset -p yes"
+              sh "docker buildx create --use"
+              sh "docker buildx inspect --bootstrap"
               sh "cd `pwd` && DOCKER_CLI_EXPERIMENTAL=enabled DOCKER_BUILDKIT=1 docker build --platform linux/arm64 -t docker.io/vikaspogu/${imageName} ${subFolder}"
           }
         }
