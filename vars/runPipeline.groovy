08082af45c37121899e90a9a473012bd8398729b
vikaspogu <vikaspoguadf@gmail.com>
2020-09-07 14:36:45 -0500

    test buildx

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -29,6 +29,9 @@ def call(body) {
         steps{
           container("docker") {
               sh "docker run --rm --privileged multiarch/qemu-user-static --reset -p yes"
+              sh "DOCKER_CLI_EXPERIMENTAL=enabled DOCKER_BUILDKIT=1 docker buildx create --name mybuilder"
+              sh "DOCKER_CLI_EXPERIMENTAL=enabled DOCKER_BUILDKIT=1 docker buildx use mybuilder"
+              sh "DOCKER_CLI_EXPERIMENTAL=enabled DOCKER_BUILDKIT=1 docker buildx inspect --bootstrap"
               sh "cd `pwd` && DOCKER_CLI_EXPERIMENTAL=enabled DOCKER_BUILDKIT=1 docker build --platform linux/arm64 -t docker.io/vikaspogu/${imageName} ${subFolder}"
           }
         }
