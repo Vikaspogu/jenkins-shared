1c01ebe1fe22f896a9d0592105a9efd22d7ef235
vikaspogu <vikaspoguadf@gmail.com>
2020-09-07 14:59:41 -0500

    test another docker run

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -29,7 +29,7 @@ def call(body) {
         steps{
           container("docker") {
               sh "docker run --rm --privileged multiarch/qemu-user-static --reset -p yes"
-              sh "DOCKER_CLI_EXPERIMENTAL=enabled docker buildx ls"
+              sh "docker run --rm --privileged docker/binfmt:66f9012c56a8316f9244ffd7622d7c21c1f6f28d"
               sh "cd `pwd` && DOCKER_CLI_EXPERIMENTAL=enabled DOCKER_BUILDKIT=1 docker build --platform linux/arm64 -t docker.io/vikaspogu/${imageName} ${subFolder}"
           }
         }
