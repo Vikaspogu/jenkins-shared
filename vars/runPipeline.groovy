18a7a7a216b429d7d6adb972696f4749c48cae1f
vikaspogu <vikaspoguadf@gmail.com>
2020-09-29 10:11:07 -0500

    remove subfolder

--- vars/runPipeline.groovy
+++ vars/runPipeline.groovy
@@ -10,7 +10,6 @@ def call(body) {
   def imageName = "${pipelineParams.imageName}"
   def namespace = "${pipelineParams.namespace}"
   def deploymentName = "${pipelineParams.deploymentName}"
-  def subFolder = "${pipelineParams.subFolder}" ?: "."
 
   pipeline {
     agent {
@@ -30,7 +29,7 @@ def call(body) {
           container("docker") {
               sh "docker run --rm --privileged multiarch/qemu-user-static --reset -p yes"
               sh "docker run --rm --privileged docker/binfmt:66f9012c56a8316f9244ffd7622d7c21c1f6f28d"
-              sh "cd `pwd` && DOCKER_CLI_EXPERIMENTAL=enabled DOCKER_BUILDKIT=1 docker build --platform linux/arm64 -t docker.io/vikaspogu/${imageName} ${subFolder}"
+              sh "cd `pwd` && DOCKER_CLI_EXPERIMENTAL=enabled DOCKER_BUILDKIT=1 docker build --platform linux/arm64 -t docker.io/vikaspogu/${imageName} ."
           }
         }
       }
